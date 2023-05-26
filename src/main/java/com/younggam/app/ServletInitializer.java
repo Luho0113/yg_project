d358dfee1758f946bf3db557726b171308e271ce
Luho0113 <luho0113@gmail.com>
2023-05-26 19:06:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/younggam/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.younggam.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YoungGamProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/com/younggam/app/YoungGamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younggam.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungGamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
