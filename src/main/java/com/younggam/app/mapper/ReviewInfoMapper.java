e39cf3ae6d39b53d7ae85659dbba6dc23b398706
Luho0113 <luho0113@gmail.com>
2023-05-30 15:02:44 +0900

    sqlsessiontemplate 오류 해결

--- src/main/java/com/younggam/app/mapper/ReviewInfoMapper.java
+++ src/main/java/com/younggam/app/mapper/ReviewInfoMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.younggam.app.vo.ReviewInfoVO;
 
-@Mapper
+//@Mapper
 public interface ReviewInfoMapper {
 	
 	List<ReviewInfoVO> selectReviewInfos(ReviewInfoVO reivews);
