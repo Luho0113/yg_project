c991ac397e38ffd65a45bd5de3e08fd9094c6764
comput3202 <comput3203@gmail.com>
2023-06-09 13:07:51 +0900

    test

--- src/main/java/com/younggam/app/controller/MovieController.java
+++ src/main/java/com/younggam/app/controller/MovieController.java
@@ -22,15 +22,16 @@ public class MovieController {
 		return "movie/search";
 	}
 	
-	@GetMapping("/credit")
-	public String getCredit(@RequestParam("movieId") String movieId, Model m) {
-		m.addAttribute("credit", movieService.getCredit(movieId)); //배열을 전달하면 movie에 담긴 상태로 jsp 파일로 이동한다. (jsp 파일에서 for each문을 돌려 배열의 값을 꺼내면 된다.)
-		return "movie/search";
-	}
+//	@GetMapping("/credit")
+//	public String getCredit(@RequestParam("movieId") String movieId, Model m) {
+//		m.addAttribute("credit", movieService.getCredit(movieId)); //배열을 전달하면 movie에 담긴 상태로 jsp 파일로 이동한다. (jsp 파일에서 for each문을 돌려 배열의 값을 꺼내면 된다.)
+//		return "movie/search";
+//	}
+//배우와 감독을 출력하기 위해서 만들었으나 현재 영화 검색 페이지(=search)에서 조회 가능하기 때문에 삭제 보류(필요 없으면 삭제 하3~)
 	
 	@GetMapping("/detail")
 	public String getDetail() {
-		return "movie/detail";
+		return "movie/detai";
 	}
 	
 	
