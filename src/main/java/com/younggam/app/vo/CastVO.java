d0edfd147fd9ae5554fcb020af3c8c26fe3ca068
comput3202 <comput3203@gmail.com>
2023-06-12 17:49:56 +0900

    CastVO

---
+++ src/main/java/com/younggam/app/vo/CastVO.java
@@ -0,0 +1,12 @@
+package com.younggam.app.vo;
+
+import lombok.Data;
+
+@Data
+public class CastVO {
+	
+	private String actor; //배우명
+	private String character; //배역명
+	private String porfilePath; //프로필 이미지
+
+}
